954c0b102e1d5f7f4183eefa447e4043b4560d96
Tony Crisci <tony@dubstepdish.com>
2019-02-02 22:33:40 -0500

    use BigInt by default for 'x' and 't'
    (Breaking change)
    
    Always use BigInt to represent dbus int64 and uint64 types.
    
    Remove Long.js dependency.

---
+++ test/integration/long.test.js
@@ -0,0 +1,78 @@
+// Test that methods and properties of type 'x' (long int) work correctly
+
+let dbus = require('../../');
+
+let {
+  Interface, property, method, signal,
+  ACCESS_READ, ACCESS_WRITE, ACCESS_READWRITE
+} = dbus.interface;
+
+let {
+  MAX_INT64, MIN_INT64,
+  MAX_UINT64, MIN_UINT64
+} = require('../../lib/constants');
+
+const TEST_NAME = 'org.test.name';
+const TEST_PATH = '/org/test/path';
+const TEST_IFACE = 'org.test.iface';
+
+let bus = dbus.sessionBus();
+
+class LongInterface extends Interface {
+  @method({inSignature: 'x', outSignature: 'x'})
+  EchoSigned(what) {
+    return what;
+  }
+
+  @method({inSignature: 't', outSignature: 't'})
+  EchoUnsigned(what) {
+    return what;
+  }
+}
+
+let testIface = new LongInterface(TEST_IFACE);
+
+beforeAll(async () => {
+  await bus.export(TEST_NAME, TEST_PATH, testIface);
+});
+
+afterAll(() => {
+  bus.connection.stream.end();
+});
+
+test('test long type works correctly', async () => {
+  let object = await bus.getProxyObject(TEST_NAME, TEST_PATH);
+  let test = object.getInterface(TEST_IFACE);
+
+  // small numbers
+  let what = -30n;
+  let result = await test.EchoSigned(what);
+  // XXX jest does not support bigint yet
+  expect(result === what).toEqual(true);
+
+  what = 30n;
+  result = await test.EchoUnsigned(what);
+  expect(result === what).toEqual(true);
+  result = await test.EchoSigned(what);
+  expect(result === what).toEqual(true);
+
+  // int64 max
+  what = MAX_INT64;
+  result = await test.EchoSigned(what);
+  expect(result === what).toEqual(true);
+
+  // int64 min
+  what = MIN_INT64;
+  result = await test.EchoSigned(what);
+  expect(result === what).toEqual(true);
+
+  // uint64 max
+  what = MAX_UINT64;
+  result = await test.EchoUnsigned(what);
+  expect(result === what).toEqual(true);
+
+  // uint64 min
+  what = MIN_UINT64;
+  result = await test.EchoUnsigned(what);
+  expect(result === what).toEqual(true);
+});
