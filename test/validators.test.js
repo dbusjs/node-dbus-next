c543ca1e02b24135a5fafb7e9718f8b3d148db8d
Tony Crisci <tony@dubstepdish.com>
2019-02-11 06:34:34 -0500

    refactor tests and fix validator return type

---
+++ test/validators.test.js
@@ -0,0 +1,41 @@
+const {
+  isObjectPathValid,
+  isInterfaceNameValid,
+  isMemberNameValid
+} = require('../lib/validators');
+
+test('object path validators', () => {
+  let validPaths = [ '/', '/foo', '/foo/bar', '/foo/bar/bat' ];
+  for (path of validPaths) {
+    expect(isObjectPathValid(path)).toBe(true);
+  }
+
+  let invalidPaths = [ undefined, {}, '', 'foo', 'foo/bar', '/foo/bar/', '/$/foo/bar', '/foo//bar', '/foo$bar/baz' ];
+  for (path of invalidPaths) {
+    expect(isObjectPathValid(path)).toBe(false);
+  }
+});
+
+test('bus name validators', () => {
+  let validNames = [ 'foo.bar', 'foo.bar.bat', '_foo._bar', 'foo.bar69' ];
+  for (name of validNames) {
+    expect(isInterfaceNameValid(name)).toBe(true);
+  }
+
+  let invalidNames = [ undefined, {}, '', '5foo.bar', 'foo.6bar', '.foo.bar', 'bar..baz', '$foo.bar', 'foo$.ba$r' ];
+  for (name of invalidNames) {
+    expect(isInterfaceNameValid(name)).toBe(false);
+  }
+});
+
+test('member name validators', () => {
+  let validMembers = [ 'foo', 'FooBar', 'Bat_Baz69' ];
+  for (member of validMembers) {
+    expect(isMemberNameValid(member)).toBe(true);
+  }
+
+  let invalidMembers = [ undefined, {}, '', 'foo.bar', '5foo', 'foo$bar' ];
+  for (member of invalidMembers) {
+    expect(isMemberNameValid(member)).toBe(false);
+  }
+});
